1fdecbe084e8de0a05339f99f5ba92ed1e343b42
alexa <alexanderredar1@gmail.com>
2023-07-24 16:19:05 +0300

    [7] - Bean scope

--- src/spring_introduction/Dog.java
+++ src/spring_introduction/Dog.java
@@ -2,6 +2,8 @@ package spring_introduction;
 
 public class Dog implements Pet {
 
+    private String name;
+
     public Dog() {
         System.out.println("Dog bean is created");
     }
@@ -9,5 +11,13 @@ public class Dog implements Pet {
     @Override
     public void say() {
         System.out.println("Bow-Wow");
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 }
