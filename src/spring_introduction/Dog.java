c96c474b9f73af37e59f6773255101d263227267
alexa <alexanderredar1@gmail.com>
2023-07-24 21:22:07 +0300

    [16] - Аннотации @Scope, @PostConstruct и @PreDestroy

--- src/spring_introduction/Dog.java
+++ src/spring_introduction/Dog.java
@@ -1,6 +1,10 @@
 package spring_introduction;
 
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
 
 @Component
 public class Dog implements Pet {
@@ -16,10 +20,12 @@ public class Dog implements Pet {
         System.out.println("Bow-Wow");
     }
 
+    @PostConstruct
     public void init() {
         System.out.println("Class Dog: init method");
     }
 
+    @PreDestroy
     public void destroy() {
         System.out.println("Class Dog: destroy method");
     }
