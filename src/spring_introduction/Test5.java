c2a53f1ba8d3142d2ead5d67cd6dfc454c6ceea4
alexa <alexanderredar1@gmail.com>
2023-07-24 17:31:45 +0300

    [9] - Методы init и destroy

---
+++ src/spring_introduction/Test5.java
@@ -0,0 +1,20 @@
+package spring_introduction;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Test5 {
+    public static void main(String[] args) {
+        ClassPathXmlApplicationContext context =
+                new ClassPathXmlApplicationContext("applicationContext2.xml");
+
+        Dog myDog = context.getBean("myPet", Dog.class);
+        myDog.say();
+        myDog.destroy();
+
+        Dog yourDog = context.getBean("myPet", Dog.class);
+        yourDog.say();
+        yourDog.destroy();
+
+        context.close();
+    }
+}
