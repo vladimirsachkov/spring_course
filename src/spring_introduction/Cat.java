29a6b857f98ee4c290a4a5c55fba450b4d7fad67
alexa <alexanderredar1@gmail.com>
2023-07-24 17:48:38 +0300

    [10] -  Конфигурация с помощью аннотаций

--- src/spring_introduction/Cat.java
+++ src/spring_introduction/Cat.java
@@ -1,5 +1,8 @@
 package spring_introduction;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class Cat implements Pet {
     public Cat() {
         System.out.println("Cat bean is created");
---
+++ src/spring_introduction/ConfigWithAnnotations1.java
@@ -0,0 +1,15 @@
+package spring_introduction;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class ConfigWithAnnotations1 {
+    public static void main(String[] args) {
+        ClassPathXmlApplicationContext context =
+                new ClassPathXmlApplicationContext("applicationContext3.xml");
+
+        Cat myCat = context.getBean("cat", Cat.class);
+        myCat.say();
+
+        context.close();
+    }
+}
